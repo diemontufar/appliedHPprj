6c89690d3e70fa7a4f79fc587aae835f826e4503
Diego Montúfar <diogonal0@gmail.com>
2014-10-22 23:42:25 +1100

    Report Update

--- Assignment2/src/Assignment2_cpp/src/Assignment2_MPI.cpp
+++ Assignment2/src/Assignment2_cpp/src/Assignment2_MPI.cpp
@@ -437,10 +437,8 @@ void	assembleSystem(SparseMatrix& M, SparseMatrix& K, double* s, double* phi, bo
     M.initialize(myN_p, 10);
     K.initialize(myN_p, 10);
 
-    for(int e=0; e<myN_e; e++)
-    {
-        for(int p=0; p<4; p++)
-        {
+    for(int e=0; e<myN_e; e++){
+        for(int p=0; p<4; p++){
             Nodes[p]= Elements[e][p];
             x[p]	= Points[Nodes[p]][0];
             y[p]	= Points[Nodes[p]][1];
@@ -451,28 +449,24 @@ void	assembleSystem(SparseMatrix& M, SparseMatrix& K, double* s, double* phi, bo
 		gradEta[0][1] = ((y[2]-y[0])*(z[3]-z[2])-(y[2]-y[3])*(z[0]-z[2]))/(6*Omega[e]);
 		gradEta[0][2] = ((y[1]-y[3])*(z[0]-z[3])-(y[0]-y[3])*(z[1]-z[3]))/(6*Omega[e]);
 		gradEta[0][3] = ((y[0]-y[2])*(z[1]-z[0])-(y[0]-y[1])*(z[2]-z[0]))/(6*Omega[e]);
-
 		gradEta[1][0] = ((x[2]-x[1])*(z[3]-z[1])-(x[3]-x[1])*(z[2]-z[1]))/(6*Omega[e]);
 		gradEta[1][1] = ((x[3]-x[2])*(z[2]-z[0])-(x[0]-x[2])*(z[2]-z[3]))/(6*Omega[e]);
 		gradEta[1][2] = ((x[0]-x[3])*(z[1]-z[3])-(x[1]-x[3])*(z[0]-z[3]))/(6*Omega[e]);
 		gradEta[1][3] = ((x[1]-x[0])*(z[0]-z[2])-(x[2]-x[0])*(z[0]-z[1]))/(6*Omega[e]);
-
 		gradEta[2][0] = ((x[3]-x[1])*(y[2]-y[1])-(x[2]-x[1])*(y[3]-y[1]))/(6*Omega[e]);
 		gradEta[2][1] = ((x[2]-x[0])*(y[3]-y[2])-(x[2]-x[3])*(y[0]-y[2]))/(6*Omega[e]);
 		gradEta[2][2] = ((x[1]-x[3])*(y[0]-y[3])-(x[0]-x[3])*(y[1]-y[3]))/(6*Omega[e]);
 		gradEta[2][3] = ((x[0]-x[2])*(y[1]-y[0])-(x[0]-x[1])*(y[2]-y[0]))/(6*Omega[e]);
 
         // Outer loop over each node
-        for(int p=0; p<4; p++)
-        {
+        for(int p=0; p<4; p++){
             m		= Nodes[p];
             gradEta_p[0]	= gradEta[0][p];
             gradEta_p[1]	= gradEta[1][p];
             gradEta_p[2]	= gradEta[2][p];
 
             // Inner loop over each node
-            for(int q=0; q<4; q++)
-            {
+            for(int q=0; q<4; q++){
                 n			= Nodes[q];
                 gradEta_q[0]		= gradEta[0][q];
                 gradEta_q[1]		= gradEta[1][q];
